6bdccc2b2781a87127675429def0af22839f2c3e
Alair Fonseca <alairfonseca13@gmail.com>
2017-05-05 20:03:13 -0300

    CrossOrigin
    adicionando cross origin

--- simplex-service/src/main/java/me/alair/simplex/web/SimplexServiceController.java
+++ simplex-service/src/main/java/me/alair/simplex/web/SimplexServiceController.java
@@ -1,6 +1,9 @@
 package me.alair.simplex.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,12 +18,13 @@ public class SimplexServiceController {
 	@Autowired
 	VenttselSimplex venttselSimplexService;
 
+	@CrossOrigin
 	@RequestMapping(value = "/simplex", method = RequestMethod.POST)
-	public String simplex(@RequestBody FuncaoOtima funcaoObjetivo) {
+	public ResponseEntity<String> simplex(@RequestBody FuncaoOtima funcaoObjetivo) {
 
-		
+		String resposta = venttselSimplexService.executaSimplex(funcaoObjetivo);
 
-		return venttselSimplexService.executaSimplex(funcaoObjetivo);
+		return ResponseEntity.status(HttpStatus.OK).body(resposta);
 	}
 
 }
